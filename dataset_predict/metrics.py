5136f4c97b44036c0089c46d4304282f90058a0b
kamil <kammmilkos@gmail.com>
2018-05-28 22:03:12 +0200

    Fixed metrics

--- dataset_predict/metrics.py
+++ dataset_predict/metrics.py
@@ -1,37 +1,23 @@
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, accuracy_score, f1_score
 import numpy as np
 
 def get_metrics(true, pred):
-    cm = confusion_matrix(true, pred)
+    ACC = accuracy_score(true, pred)
+    precision = precision_score(true, pred, average='weighted')
+    recall = recall_score(true, pred, average='weighted')
+    F1 = f1_score(true, pred, average='weighted')
 
-    FP = np.mean(cm.sum(axis=0) - np.diag(cm))
-    FN = np.mean(cm.sum(axis=1) - np.diag(cm))
-    TP = np.mean(np.diag(cm))
-    TN = np.mean(cm.sum() - (FP + FN + TP))
-
-    Pos = TP+FN
-    Neg = TN+FP
-
-    TPR = TP/Pos #recall
-    TNG = TN/Neg
-    FPR = 1 - TNG
-
-    ACC = (TP+TN)/(Pos+Neg)
-
-    precision = TP/(TP+FP)
-    F1=2*(TPR*precision)/(TPR+precision)
-
-    return [ACC, TPR, FPR, precision, TPR, F1]
+    return [ACC, precision, recall, F1]
 
 def print_metrics(metrics):
-    print("Accuracy: {0[0]}, TP Rate: {0[1]}, FP Rate: {0[2]}, Precision: {0[3]}, Recall: {0[4]}, F1-Score: {0[5]}\n".format(metrics))
+    print("Accuracy: {0[0]}, Precision: {0[1]}, Recall: {0[2]}, F1-Score: {0[3]}\n".format(metrics))
 
 def get_metrics_full(true, pred, mean=True):
     metrics = np.array([])
     for i in range(len(true)):
         metrics = np.concatenate((metrics, get_metrics(true[i], pred[i])), axis=0)
 
-    metrics = metrics.reshape(len(true), 6)
+    metrics = metrics.reshape(len(true), 4)
 
     if mean:
         return np.mean(metrics, axis=0)
