3836e60c17612c7872f5c6ae51ff221579945ef2
kamil <kammmilkos@gmail.com>
2019-03-24 19:09:17 +0100

    Update sarimax.py - added model params and use every 10th element in fit/predict

--- sarimax.py
+++ sarimax.py
@@ -4,11 +4,17 @@ import argparse
 from sklearn.preprocessing import LabelEncoder
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import dataset_predict.metrics
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+# https://www.kaggle.com/poiupoiu/how-to-use-sarimax
+# https://www.statsmodels.org/dev/generated/statsmodels.tsa.statespace.sarimax.SARIMAX.html
+# https://machinelearningmastery.com/sarima-for-time-series-forecasting-in-python/
 
 test_data_percent = 10
 
 #order = (p, d, q)
-SARIMAX_order = (1, 0, 0)
+SARIMAX_order = (7, 0, 16)
 
 
 def encode_values(df):
@@ -28,6 +34,7 @@ def main():
 
     # load dataset
     df = pd.read_csv(args.input)
+    df = df[::10]
     encoded_df, classes = encode_values(df)
     series = encoded_df[:, (encoded_df.shape[1] - 1):]
     exog = encoded_df[:, 0:(encoded_df.shape[1] - 1)]
@@ -39,6 +46,14 @@ def main():
     exog_test = exog[len(exog_train):, :]
 
     print('Loaded data: ', args.input, '\nseries_train.shape: ', series_train.shape, '\nseries_test.shape: ', series_test.shape, '\nexog_train.shape: ', exog_train.shape, '\nexog_test.shape: ', exog_test.shape)
+
+    #fig, ax = plt.subplots(2, 1, figsize=(20, 10))
+    #fig = sm.graphics.tsa.plot_acf(pd.DataFrame(series_train).diff(), lags=25, ax=ax[0])
+    #fig = sm.graphics.tsa.plot_pacf(pd.DataFrame(series_train).diff(), lags=25, ax=ax[1])
+    #plt.show()
+
+    #resDiff = sm.tsa.arma_order_select_ic(series_train, max_ar=20, max_ma=20, ic='aic', trend='nc')
+    #print('ARMA(p,q) =', resDiff['aic_min_order'], 'is the best.')
 
     model = SARIMAX(endog=series_train, exog=exog_train, order=SARIMAX_order, enforce_stationarity=False, enforce_invertibility=False)
     model_fitted = model.fit(disp=1)
